0b23644010be4f9768624c8c9825cf260c0dd0c7
Bikram Choudhury <bikram.choudhury@ad.infosys.com>
2020-04-26 01:38:52 +0800

    [IMPL] Render admin route components at Admin layout

--- src/layout/Admin/Admin.jsx
+++ src/layout/Admin/Admin.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
+import RouteConfig from '../../route';
 import NavBar from "../../components/NavBar/NavBar";
 import SideBar from "../../components/SideBar/SideBar";
 import LoggedInFooter from "../../components/LoggedInFooter/LoggedInFooter";
 
+export function createAdminRoutes(routerConfigList) {
+	const routeElements = [];
+	routerConfigList.forEach(config => {
+		const routes = config.routes;
+		routes.forEach(route => {
+			if (route.subRoutes) {
+				route.subRoutes.forEach(subRoute => {
+					routeElements.push(
+						<Route
+							key={subRoute.name}
+							path={route.path + subRoute.path}
+							component={subRoute.component}
+						/>
+					);
+				});
+			} else {
+				routeElements.push(
+					<Route key={route.name} path={route.path} component={route.component} />
+				);
+			}
+		});
+	});
+	return routeElements;
+}
+
 const Admin = props => {
+	const adminRoutes = createAdminRoutes(RouteConfig);
 	return (
 		<div id="page-top">
 			<div id="wrapper">
@@ -12,7 +40,9 @@ const Admin = props => {
 					<div id="content">
 						<NavBar />
 						<div className="container-fluid">
-							{/* <!-- Main Content --> */}
+							<Switch>
+								{adminRoutes}
+							</Switch>
 						</div>
 					</div>
 					<LoggedInFooter />
--- src/layout/Admin/spec.jsx
+++ src/layout/Admin/spec.jsx
@@ -1,17 +1,60 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Admin from "./Admin";
+import Admin, { createAdminRoutes } from "./Admin";
+
+const TestComponent = () => (<div>Test component</div>);
 
 describe("Admin", () => {
 	let component;
 	beforeEach(() => {
 		component = shallow(<Admin />);
-    });
-    it("should render", () => {
+	});
+	it("should render", () => {
 		expect(component.find("div")).toBeTruthy();
 		expect(component.find("SideBar")).toBeTruthy();
 		expect(component.find("NavBar")).toBeTruthy();
 		expect(component.find(".container-fluid")).toBeTruthy();
 		expect(component.find("LoggedInFooter")).toBeTruthy();
+	});
+	describe('should create routes', () => {
+		it('should not have sub routes', () => {
+			const name = 'Dashboard';
+			const RouterList = [{
+				routes: [{
+					path: '/',
+					name: name,
+					component: TestComponent
+				}]
+			}];
+			const routerElemets = createAdminRoutes(RouterList);
+			const routeEl = routerElemets[ 0 ];
+
+			expect(routerElemets).toHaveLength(1);
+			expect(routeEl.key).toEqual(name);
+		});
+		it('should not have sub routes', () => {
+
+			const RouterList = [{
+				routes: [{
+					path: '/test',
+					name: 'Dashboard',
+					subRoutes: [{
+						path: '/buttons',
+						name: 'Buttons',
+						component: TestComponent
+					}, {
+						path: '/cards',
+						name: 'Cards',
+						component: TestComponent
+					}]
+				}]
+			}];
+			const routerElemets = createAdminRoutes(RouterList);
+			const routeEl = routerElemets[ 0 ];
+			
+			expect(routerElemets).toHaveLength(2);
+			expect(routeEl.key).toEqual('Buttons');
+			expect(routeEl.props.path).toEqual('/test/buttons');
+		});
 	});
 });
